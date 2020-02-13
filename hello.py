40d1f6ae2099c4956b4efd395e40288e8489f6c4
Mark Hammond <mahinbox@hotmail.com>
2020-02-12 19:44:23 -0500

    initial_proj

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/<name>')
+
+def index(name):
+    body = '<h1>Hello World {}!</h1>'.format(name)
+    body = body + '<p>This is the body after the header...<p>'
+    body = body + '<b><u>and after that...</b></u>'
+    return body
