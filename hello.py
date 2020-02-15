485a8e6cc551840dad14bebff4dc04bd0a4800ac
Mark Hammond <mahinbox@hotmail.com>
2020-02-15 09:25:50 -0500

    Added another line...

--- hello.py
+++ hello.py
@@ -8,5 +8,6 @@ def index(name):
     body = '<h1>Hello World {}!</h1>'.format(name)
     body = body + '<p>This is the body after the header...<p>'
     body = body + '<b><u>and after that...</b></u>'
-    body = body + '<p><b><ui>and then the line with italics</i></u>'
+    body = body + '<p><u><i>and then the line with italics</i></u>'
+    body = body + '<p>and now a line with no special formatting</u>'
     return body
